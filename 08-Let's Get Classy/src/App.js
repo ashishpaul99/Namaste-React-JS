8c07e9aa980a3350d3ec2ec08c75fbe3f38ab452
GADDALA ASHISH <ashishpaul1199@gmail.com>
2025-03-23 19:47:52 +0530

    Add files via upload

---
+++ 08-Let's Get Classy/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body"
+import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+
+
+const AppLayout=()=>{
+    return(
+        <div className="app">
+          <Header/>
+          <Outlet/>
+        </div>
+    );
+};
+
+// Creating Routing Configuration
+const appRouter = createBrowserRouter([
+  {
+      path:"/",
+      element:<AppLayout/>, 
+      
+      children:[ {
+        path:"/",
+        element:<Body/>,
+      },
+      {
+        path:"/about",
+        element:<About/>,    
+      },{
+        path:"/contact",
+        element:<Contact/>,
+      },
+      {
+        path:"/restaurant/:resId",
+        element:<RestaurantMenu/>
+      }
+    ],
+      errorElement:<Error/>,
+  },
+ 
+]);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+// passing router configuration
+root.render(<RouterProvider router={appRouter}/>);
+
