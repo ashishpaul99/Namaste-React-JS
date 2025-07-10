0296810ac476b5c4418329a378c01b2075c52640
GADDALA ASHISH <ashishpaul1199@gmail.com>
2025-07-10 21:36:10 +0530

    Add files via upload

---
+++ 12-Let's Build Our Store/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/index.html", // Correct path for the HTML file
+    "./src/**/*.{js,jsx,ts,tsx}", // Correct path for all React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
